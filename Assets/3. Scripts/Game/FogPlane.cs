8bb5897d96f411c776da8e1b5510083a4acf95e9
whitebbit <66116064+whitebbit@users.noreply.github.com>
2023-11-27 16:49:00 +0300

    Update FogPlane.cs

--- Assets/3. Scripts/Game/FogPlane.cs
+++ Assets/3. Scripts/Game/FogPlane.cs
@@ -6,10 +6,12 @@ namespace _3._Scripts.Game
     [RequireComponent(typeof(MeshRenderer))]
     public class FogPlane: MonoBehaviour
     {
+        private static readonly int Property = Shader.PropertyToID("_FogColor");
+
         private void Start()
         {
             var component = GetComponent<MeshRenderer>();
-            component.material.color = GameManager.CurrentTheme.Fog;
+            component.material.SetColor(Property, GameManager.CurrentTheme.Fog);
         }
     }
 }
